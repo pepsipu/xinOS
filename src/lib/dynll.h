7bc81dba371a1790c91bb40a6ea5907eeee58cc4
Sammy Hajhamid <pokeso123@hotmail.com>
2020-08-05 02:37:49 -0700

    addding searching through dynamic lls

--- src/lib/dynll.h
+++ src/lib/dynll.h
@@ -11,18 +11,29 @@
         type data; \
     }; \
     typedef struct name##_dynll_node name##_dynll_node_t; \
-    name##_dynll_node_t name##_dynll = { &name##_dynll, &name##_dynll } \
+    name##_dynll_node_t name##_dynll = { &name##_dynll, &name##_dynll }
 
 #define prepend_dynll(name, element) ({ \
     name##_dynll_node_t *new_node = kmalloc(sizeof(name##_dynll_node_t)); \
     new_node->data = element; \
     new_node->fd = name##_dynll.fd; \
     name##_dynll.fd = new_node; \
-)} \
+})
 
 #define append_dynll(name, element) ({ \
     name##_dynll_node_t *new_node = kmalloc(sizeof(name##_dynll_node_t)); \
     new_node->data = element; \
     new_node->bk = name##_dynll.bk; \
     name##_dynll.bk = new_node; \
-)} \
+})
+
+#define search_dynll_element(name, condition) ({ \
+    name##_dynll_node_t *ptr; \
+    for (ptr = name##_dynll.fd; ptr != &name##_dynll; ptr = ptr->fd) { \
+        if (condition) \
+            goto ret; \
+    } \
+    ptr = 0; \
+    ret: \
+    ptr; \
+})
